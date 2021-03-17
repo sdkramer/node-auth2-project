819f2cf1ccfac7e82fa7988b0de686a7d94ad1b9
Sean Kramer <seandominickramer@gmail.com>
2021-03-16 21:04:52 -0400

    Add findBy() in models

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -37,6 +37,10 @@ function findBy(filter) {
       }
     ]
    */
+  return db('users as u')
+  .innerJoin('roles as r', 'u.role_id', 'r.role_id')
+  .where(filter)
+  .select('u.user_id', 'u.username', 'u.password','r.role_name')
 }
 
 function findById(user_id) {
