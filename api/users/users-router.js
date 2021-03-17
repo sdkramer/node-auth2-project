3df43f3d469e336f2f2d49c1baa5c5ea92973787
Sean Kramer <seandominickramer@gmail.com>
2021-03-16 20:58:35 -0400

    Add find() in models

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -17,7 +17,9 @@ const { restricted, only } = require("../auth/auth-middleware.js");
     }
   ]
  */
-router.get("/", restricted, (req, res, next) => { // done for you
+router.get("/", 
+// restricted, 
+(req, res, next) => { // done for you
   Users.find()
     .then(users => {
       res.json(users);
